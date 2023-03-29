7fc7e8a3425bda6bf12e457cd479d03cd6a66f86
Goddohi <kmam147@gmail.com>
2023-03-29 10:12:18 +0900

    Initial commit

---
+++ x , y.py
@@ -0,0 +1,17 @@
+inp=input()
+inpp=input()
+x = int(inp)
+y = int(inpp)
+print(x+y)
+print(x-y)
+print(x*y)
+print(x//y)
+print(x%y)
+
+
+
+
+print("\\    /\\")
+print(" )   ( ')")
+print('(   /   )')
+print(' \\ (__ )|')
